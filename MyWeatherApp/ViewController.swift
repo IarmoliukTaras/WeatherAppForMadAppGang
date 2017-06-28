b2389dae09b9037cf994d9c2bb9f6155c97bb1a2
Iarmoliuk Taras <iarmoliuktaras@gmail.com>
2017-06-28 12:05:12 +0300

    add Facebook logIn

--- MyWeatherApp/ViewController.swift
+++ MyWeatherApp/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
+    
+    let facebookButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .blue
+        button.setImage(#imageLiteral(resourceName: "facebook").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.layer.cornerRadius = button.frame.width / 2
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        
+        navigationItem.title = "My Weather App"
+        
+        setupFacebookButton()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupFacebookButton() {
+        view.addSubview(facebookButton)
+        
+        facebookButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        facebookButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        facebookButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        facebookButton.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        facebookButton.layer.cornerRadius = 100
+        facebookButton.addTarget(self, action: #selector(handleFBLogin), for: .touchUpInside)
     }
-
+    
+    func handleFBLogin() {
+        FBSDKLoginManager().logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if let err = error {
+                print("FB login failed", err)
+            }
+            print(result?.token.tokenString)
+        }
+    }
 
 }
 
